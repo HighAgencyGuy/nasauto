31f07028dacda31cd4c020aa8a9056ccc69b54e9
HighAgencyGuy <highagencyguy@gmail.com>
2026-01-29 07:31:11 -0800

    Add AdminHome component with Dashboard integration

---
+++ src/pages/admin/index.tsx
@@ -0,0 +1,14 @@
+// src/pages/admin/index.tsx
+
+import React from "react";
+import Dashboard from "./Dashboard"; // or any component you want as the main view
+
+const AdminHome = () => {
+  return (
+    <div>
+      <Dashboard />
+    </div>
+  );
+};
+
+export default AdminHome;
